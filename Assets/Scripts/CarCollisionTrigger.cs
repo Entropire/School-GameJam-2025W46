3a867dc74361b0a1012dc7c5fb36d6684c3bd2be
Entropire <158569522+Entropire@users.noreply.github.com>
2025-11-10 19:17:16 +0100

    Add CarCollisionTrigger script for collision events
    When a collision is trigger it checks if it was the target. If so it triggers a unity event.

---
+++ Assets/Scripts/CarCollisionTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class CarCollisionTrigger : MonoBehaviour
+{
+    [SerializeField] private string targetTag = "Player2";
+    [SerializeField] private UnityEvent onCollisionWithTarget;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(targetTag))
+        {
+            onCollisionWithTarget?.Invoke();
+        }
+    }
+}
