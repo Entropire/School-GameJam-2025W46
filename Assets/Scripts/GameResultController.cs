edf00bf38e6344406d5c9bb6e492ea7327d2332c
Entropire <158569522+Entropire@users.noreply.github.com>
2025-11-10 19:19:17 +0100

    Add game result controller and player enum
    GameResultController listens to the events of CountdownTimer and CarCollisionTrigger to determine witch player has won.

---
+++ Assets/Scripts/GameResultController.cs
@@ -0,0 +1,12 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameResultController : MonoBehaviour
+{
+    [SerializeField] private UnityEvent<Players> onPlayerWin;
+
+    public void HandleCollisionWithTarget() => onPlayerWin.Invoke(Players.Child);
+
+    public void HandleTimerDone() => onPlayerWin.Invoke(Players.Grandpa);
+}
