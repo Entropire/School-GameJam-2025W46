92045f740083026311e1daa23698b8834fb26b58
Entropire <158569522+Entropire@users.noreply.github.com>
2025-11-13 11:12:58 +0100

    Refactor PlayerRow prefab and remove Art scene

---
+++ Assets/Scripts/Enggame.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enggame : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
